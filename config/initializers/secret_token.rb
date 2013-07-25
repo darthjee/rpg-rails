822a83c1899b45a6976949861295f376094135b4
favini <darthjee@gmail.com>
2013-07-25 20:08:29 -0300

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RpgRails::Application.config.secret_token = '2c6e1c3fdf355c41f0382d87262fc579f1554c9141b9333d91a6ec44e3c8c06308df95a16b51740e4561655761315376f9ee0d478399081a23714089908f1376'
