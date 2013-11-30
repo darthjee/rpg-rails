d10621c6145f88405ff09abd8ac4ff5cc2b28937
darthjee <darthjee@gmail.com>
2013-11-30 18:39:59 -0200

    Read configuration from mongo.yml

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,6 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#rpg-#{Rails.env}"
+settings = YAML.load(File.read(File.expand_path('../../config/mongo.yml', File.dirname(__FILE__))))["#{Rails.env}"]
+MongoMapper.connection = Mongo::Connection.new(settings['host'], settings['port'])
+MongoMapper.database = settings['database']
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
