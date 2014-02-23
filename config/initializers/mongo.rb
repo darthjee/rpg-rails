fd1645bdbc424be1e632881790b2eab2c106b5ec
darthjee <darthjee@gmail.com>
2014-02-23 17:37:42 -0300

    Accept env variable configuration for heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,20 @@
-settings = YAML.load(File.read(File.expand_path('../../config/mongo.yml', File.dirname(__FILE__))))["#{Rails.env}"]
+#MONGOHQ_URL = 'mongodb://heroku:00832683bc23a9c01d90d08d7f456333@paulo.mongohq.com:10067/app19812522'
+#MONGOHQ_URL = ENV['MONGOHQ_URL'] if ENV.has_key? 'MONGOHQ_URL'
+#MONGOHQ_URL = 'mongodb://user:password@localhost:27017/rpg-development'
+  
+settings = if ENV.has_key? 'MONGOHQ_URL'
+  MONGOHQ_URL = ENV['MONGOHQ_URL']
+  {
+    'user' => MONGOHQ_URL.gsub(%r{^mongodb://([^:]*):.*$},"\\1"),
+    'pass' => MONGOHQ_URL.gsub(%r{^mongodb://[^:]*:([^@]*).*$},"\\1"),
+    'host' => MONGOHQ_URL.gsub(%r{^mongodb://[^@]*@([^:]*):.*$},"\\1"),
+    'port' => MONGOHQ_URL.gsub(%r{^mongodb://.*@[^:]*:([^/]*).*$},"\\1"),
+    'database' => MONGOHQ_URL.gsub(%r{^mongodb://.*@[^:]*:([^/]*).*$},"\\1")
+  }
+else
+  YAML.load(File.read(File.expand_path('../../config/mongo.yml', File.dirname(__FILE__))))["#{Rails.env}"]
+end
+
 MongoMapper.connection = Mongo::Connection.new(settings['host'], settings['port'])
 MongoMapper.database = settings['database']
 
